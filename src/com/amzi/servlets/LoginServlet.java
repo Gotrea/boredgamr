d0ad15611e0042127571d3ecada6b0dbc5cfbd89
Megan Tierney <tier0045@algonquinlive.com>
2018-01-25 16:38:45 -0500

    hi

--- src/com/amzi/servlets/LoginServlet.java
+++ src/com/amzi/servlets/LoginServlet.java
@@ -15,7 +15,6 @@ import com.amzi.dao.LoginDao;
 public class LoginServlet extends HttpServlet{
 
     private static final long serialVersionUID = 1L; 
-
     public void doPost(HttpServletRequest request, HttpServletResponse response)  
             throws ServletException, IOException {  
 
