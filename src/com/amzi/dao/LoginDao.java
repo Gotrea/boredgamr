27d37b0ccea36e76deb749f64106052079f74f0d
jeff <jeffray55@gmail.com>
2018-01-25 17:29:36 -0500

    >SKJDF

--- src/com/amzi/dao/LoginDao.java
+++ src/com/amzi/dao/LoginDao.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.amzi.dao;
 
 import java.sql.Connection;
@@ -18,7 +17,7 @@ public class LoginDao {
         String dbName = "form";
         String driver = "com.mysql.jdbc.Driver";
         String userName = "root";
-        String password = "pushmeof";
+        String password = "pushmeo";
         try {
             Class.forName(driver).newInstance();
             conn = DriverManager
@@ -59,65 +58,3 @@ public class LoginDao {
         return status;
     }
 }
-=======
-package com.amzi.dao;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public class LoginDao {
-    public static boolean validate(String name, String pass) {        
-        boolean status = false;
-        Connection conn = null;
-        PreparedStatement pst = null;
-        ResultSet rs = null;
-
-        String url = "jdbc:mysql://localhost:3306/";
-        String dbName = "form";
-        String driver = "com.mysql.jdbc.Driver";
-        String userName = "meganrocks";
-        String password = "mysql";
-        try {
-            Class.forName(driver).newInstance();
-            conn = DriverManager
-                    .getConnection(url + dbName, userName, password);
-
-            pst = conn.prepareStatement("select * from login where user=? and password=?");
-            pst.setString(1, name);
-            pst.setString(2, pass);
-
-            rs = pst.executeQuery();
-            status = rs.next();
-
-        } catch (Exception e) {
-            System.out.println(e);
-        } finally {
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (pst != null) {
-                try {
-                    pst.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return status;
-    }
-}
->>>>>>> branch 'master' of https://github.com/megantierney/boredgamr
