4ba0997da84c48dfe2055f1b5e3a6011fa4b3643
jeff <jeffray55@gmail.com>
2018-01-25 17:17:50 -0500

    my pass

--- src/com/amzi/dao/LoginDao.java
+++ src/com/amzi/dao/LoginDao.java
@@ -17,7 +17,7 @@ public class LoginDao {
         String dbName = "form";
         String driver = "com.mysql.jdbc.Driver";
         String userName = "root";
-        String password = "G@mer4life";
+        String password = "pushmeoff";
         try {
             Class.forName(driver).newInstance();
             conn = DriverManager
