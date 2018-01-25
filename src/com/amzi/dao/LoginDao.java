38f00644f362f5c199e89737c7cf5a35791d0d33
jeff <jeffray55@gmail.com>
2018-01-25 18:36:41 -0500

    add jeffdeleteme

--- src/com/amzi/dao/LoginDao.java
+++ src/com/amzi/dao/LoginDao.java
@@ -1,11 +1,5 @@
 package com.amzi.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 public class LoginDao {
     public static boolean validate(String name, String pass) {        
         boolean status = false;
